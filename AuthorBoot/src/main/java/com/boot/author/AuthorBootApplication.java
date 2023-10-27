b2c6d1a734f17062935e20490893c22f36fbc7f2
ShivashisBhowmick23 <shiashis.bhowmick@outlook.com>
2023-10-27 23:52:21 +0530

    Initial commit

---
+++ AuthorBoot/src/main/java/com/boot/author/AuthorBootApplication.java
@@ -0,0 +1,13 @@
+package com.boot.author;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthorBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthorBootApplication.class, args);
+	}
+
+}
---
+++ AuthorBoot/src/test/java/com/boot/author/AuthorBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boot.author;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthorBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
